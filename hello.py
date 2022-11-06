aa09dbf41fdc76ebbb82ddf32748899472b6756d
Gabrielle <gageorge@pdx.edu>
2022-11-06 13:04:26 -0800

    put my dumb changes in a branch

--- hello.py
+++ hello.py
@@ -7,7 +7,7 @@ PORT = 6667  # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b"CAP LS")
+    s.sendall(b"CAP LS\r\n")
     data = s.recv(1024)
 
 print(f"Received {data!r}")
